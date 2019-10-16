73b56c1e8c87890018854ca7053d129cf64cfb89
Nicholas Chedid <Nicholas@Nicholass-MacBook-Pro.local>
2019-10-16 15:20:37 +0100

    Refactor some JS code and fixed some CSS code

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# Unit-Converter
-Javascript Calculator to Convert Kilos to Grams, Pounds and Stones.
+# Javascript Calculators
 
-This was my first project after learning javascript. It's a simple calculator that converts kilos to grams, pounds and stone.
+Javascript Calculators
+
+This was my first project after learning javascript.
+
+I coded three calculators. A unit converter, a Bmi calculator and a Tip calculator.
+
+I learned how to handle inputs from the user and some DOM Manipulation
 
 You can check the demo here: https://nicholasfc.github.io/Unit-Converter/.
--- app.js
+++ app.js
@@ -1,43 +1,53 @@
-document.getElementById("kilo-input").addEventListener("input", val => {
+//variables
+
+//Kilo Converter Variables
+const kiloInput = document.getElementById("kilo-input");
+const gramResult = document.getElementById("grams-result");
+const poundResult = document.getElementById("pounds-result");
+const stoneResult = document.getElementById("stones-result");
+
+//BMI Variables
+const bmiCalculate = document.getElementById("bmi-calculate");
+const weightInput = document.getElementById("weight-input");
+const heightInput = document.getElementById("height-input");
+const bmiResult = document.getElementById("bmi-result");
+
+//Tip Variables
+const tipCalculate = document.getElementById("tip-calculate");
+const tipResult = document.getElementById("tip-result");
+
+//Kilo Converter
+kiloInput.addEventListener("input", val => {
   let kilos = val.target.value;
-  document.getElementById("grams-result").innerHTML = kilos * 1000;
-  document.getElementById("pounds-result").innerHTML = parseFloat(
-    kilos * 2.20462262185
-  ).toFixed(2);
-  document.getElementById("stones-result").innerHTML = parseFloat(
-    kilos / 6.35029318
-  ).toFixed(2);
+  gramResult.innerHTML = kilos * 1000;
+  poundResult.innerHTML = parseFloat(kilos * 2.20462262185).toFixed(2);
+  stoneResult.innerHTML = parseFloat(kilos / 6.35029318).toFixed(2);
 });
 
-const bmi = document
-  .getElementById("bmi-calculate")
-  .addEventListener("click", () => {
-    let weight = document.getElementById("weight-input").value;
-    let height = document.getElementById("height-input").value;
-    let calc = (weight / (height * height)).toFixed(1);
-    document.getElementById("bmi-result").innerHTML = `Your BMI is ${calc}`;
+//BMI Calculator
+const bmi = bmiCalculate.addEventListener("click", () => {
+  const weight = weightInput.value;
+  const height = heightInput.value;
+  let calc = (weight / (height * height)).toFixed(1);
+  bmiResult.innerHTML = `Your BMI is ${calc}`;
 
-    if (calc >= 18.5 && calc <= 24.9) {
-      document.getElementById("bmi-result").style.backgroundColor =
-        "lightgreen";
-    } else if (calc >= 30) {
-      document.getElementById("bmi-result").style.backgroundColor =
-        "rgb(230, 53, 53)";
-    } else {
-      document.getElementById("bmi-result").style.backgroundColor =
-        "lightgoldenrodyellow";
-    }
-    document.getElementById("bmi-result").style.visibility = "visible";
-  });
+  if (calc >= 18.5 && calc <= 24.9) {
+    bmiResult.style.backgroundColor = "lightgreen";
+  } else if (calc >= 30) {
+    bmiResult.style.backgroundColor = "rgb(230, 53, 53)";
+  } else {
+    bmiResult.style.backgroundColor = "lightgoldenrodyellow";
+  }
+  bmiResult.style.visibility = "visible";
+});
 
-const tip = document
-  .getElementById("tip-calculate")
-  .addEventListener("click", () => {
-    let bill = document.getElementById("bill-total").value;
-    let percentage = document.getElementById("percentage").value;
+//Tip Calculator
+const tip = tipCalculate.addEventListener("click", () => {
+  let bill = document.getElementById("bill-total").value;
+  let percentage = document.getElementById("percentage").value;
 
-    let calc = (bill * (percentage / 100)).toFixed(1);
+  let calc = (bill * (percentage / 100)).toFixed(1);
 
-    document.getElementById("tip-result").innerHTML = `You tip is ${calc}$`;
-    document.getElementById("tip-result").style.visibility = "visible";
-  });
+  tipResult.innerHTML = `You tip is ${calc}$`;
+  tipResult.style.visibility = "visible";
+});
